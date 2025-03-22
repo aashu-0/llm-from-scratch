aa59f9b650c4bda95bffe8b8ddebaae23396096a
aashu <157953935+aashu-0@users.noreply.github.com>
2025-03-22 19:46:42 +0530

    add generate function

--- llm_book_notes/GPT.py
+++ llm_book_notes/GPT.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 from Transformer import TransformerBlock, LayerNorm
 
-
 #GPT model
 class GPTModel(nn.Module):
     def __init__(self, config):
@@ -51,3 +50,34 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
         idx = torch.cat((idx, idx_next), dim=1)  # shape -> (batch, n_tokens +1)
 
     return idx
+
+
+# modified generate function
+def generate(model,idx, max_new_tokens, context_size, temp=0.0,
+             top_k = None, eos_id = None):
+  for _ in range(max_new_tokens):
+        # crops the current context(initial tokens) to fit model's max context size
+        idx_cond = idx[:,-context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)  # shape (batch, n_token, vocab_size)
+
+        logits = logits[:, -1, :]  # to extracts the last vector, shape -> (batch, vocab_size)
+
+        if top_k is not None:
+          topk_logits, _= torch.topk(logits, top_k)
+          logits = torch.where(condition=logits < topk_logits.min(),
+                               input=torch.tensor(-float('inf')).to(logits.device),
+                               other=logits)
+
+        if temp >0.0:
+          logits = logits/temp
+          probs = F.softmax(logits, dim=-1)
+          idx_next = torch.multinomial(probs, num_samples=1)
+        else:
+          idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+
+        if eos_id is not None and idx_next == eos_id:
+          break
+
+        idx = torch.cat((idx, idx_next), dim=1)
+  return idx
